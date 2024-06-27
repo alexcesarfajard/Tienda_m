3c7d9edbccc42e94688cccdf7f2821a05f2ec22e
alexcesarfajard <acesar23616@ufide.ac.cr>
2024-06-26 21:08:57 -0600

    Se hace conexión Firebase y se termina CRUD categorias

---
+++ src/main/java/com/tienda/demo/service/FirebaseStorageService.java
@@ -0,0 +1,20 @@
+package com.tienda.demo.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-m-d8872.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-m-d8872-firebase-adminsdk-xlzm3-34b4d700cf.json";
+}
--- src/main/java/com/tienda/demo/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/demo/service/impl/CategoriaServiceImpl.java
@@ -24,5 +24,19 @@ public class CategoriaServiceImpl
         
         return lista;
     }
-    
+
+    @Override
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
 }
